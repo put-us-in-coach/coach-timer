0b87f7ebc67008ee31f3236dd773cc3e6c48b62d
Regenal Grant <regenal@mac.com>
2017-04-10 11:11:29 -0700

    Created folders for views, controllers, models.  Create game, player, team swift. added team.xib. Naming different ViewController. Project is called Coach timer.  Currently building wireframe

---
+++ Coach Timer/Coach Timer/Team.swift
@@ -0,0 +1,9 @@
+//
+//  Team.swift
+//  Coach Timer
+//
+//  Created by Regenal Grant on 4/10/17.
+//  Copyright © 2017 Put Us In Coach. All rights reserved.
+//
+
+import Foundation
---
+++ Coach Timer/Coach Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coach Timer
+//
+//  Created by Regenal Grant on 4/10/17.
+//  Copyright © 2017 Put Us In Coach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
