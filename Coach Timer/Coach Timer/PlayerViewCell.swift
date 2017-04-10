aef8030eea448e42c64c493fa2773738afd1c0f3
Serg T <sergtsaeb@gmail.com>
2017-04-10 15:54:35 -0700

    Add PlayerViewCell xib file and constraints

---
+++ Coach Timer/Coach Timer/PlayerViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerViewCell.swift
+//  Coach Timer
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 4/10/17.
+//  Copyright © 2017 Put Us In Coach. All rights reserved.
+//
+
+import UIKit
+
+class PlayerViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
