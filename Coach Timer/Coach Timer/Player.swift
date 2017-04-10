f3694bde5cbe4670bd0d6d7151df1fe406308892
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-04-10 12:48:09 -0700

    Add player class

--- Coach Timer/Coach Timer/Player.swift
+++ Coach Timer/Coach Timer/Player.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2017 Put Us In Coach. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum FieldPosition {
+    case goalie
+    case defense
+    case forward
+    case midfield
+}
+
+class Player {
+    
+    let name: String
+    let photo: UIImage?
+    var isPresent: Bool
+    // overall game time
+    // games played? Int
+    // current play time
+    var notes: String
+    var position = Set<FieldPosition>()
+    
+    init(name:String, photo:UIImage? = nil, notes:String = "", position:Set<FieldPosition>=[]) {
+        self.name = name
+        self.photo = photo
+        self.notes = notes
+        self.position = position
+        self.isPresent = false
+    }
+    
+}
