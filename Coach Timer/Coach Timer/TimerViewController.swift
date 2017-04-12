cdedb3ce7a7964eb8ab927d9bd0e55c44bf94db6
Regenal Grant <regenal@mac.com>
2017-04-12 12:49:26 -0700

    add stop watch, via adding button and label on top of image, wrote to time differnce

---
+++ Coach Timer/Coach Timer/TimerViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TimerViewController.swift
+//  Coach Timer
+//
+//  Created by Regenal Grant on 4/12/17.
+//  Copyright © 2017 Put Us In Coach. All rights reserved.
+//
+
+import UIKit
+import CoreFoundation
+
+class TimerViewController: UIViewController {
+    
+
+
+    var seconds = 0
+    var timer = Timer()
+    var timerIsOn = false
+
+
+
+
+
+
+
+}
