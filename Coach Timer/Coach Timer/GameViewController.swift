25b23b70a96969eb4adb565fd56f78acb8f1c9a2
Serg T <sergtsaeb@gmail.com>
2017-04-11 23:06:17 -0700

    add custom collectionViewCell named activePlayerCell for GameViewController, set up the data source and delegate methods

--- Coach Timer/Coach Timer/GameViewController.swift
+++ Coach Timer/Coach Timer/GameViewController.swift
@@ -15,49 +15,47 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        let playaNib = UINib(nibName: "activePlayerCell", bundle: nil)
+        self.collectionView.register(playaNib, forCellWithReuseIdentifier: "activePlayerCell")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectionView.reloadData()
+    }
+    
+}
+    
+    
+extension GameViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Team.shared.activeRoster.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        if let view = cell as? activePlayerCell {
+            
+            view.imageView.image = Team.shared.activeRoster[indexPath.row].photo
+        }
+        return cell
+    }
+    
+    
 
-        
-    }
-    
-    
-    
-//extension GameViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    
-    //        setupDatasource()
-//        setupInitialLayout()
-        
-    }
-    
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        
-//        
-//    }
-    
-    
-//    struct itemsToDisplay {
-//    
-//    }
-//    
-//    struct ImageToDisplay {
-//        
-//    }
-//    
-//    func setupDatasource() {
-//        itemsToDisplay = [ImageToDisplay(imageName: "1"), ImageToDisplay(imageName: "2"), ImageToDisplay(imageName: "3"), ImageToDisplay(imageName: "4"),
-//                          ImageToDisplay(imageName: "5"), ImageToDisplay(imageName: "6"), ImageToDisplay(imageName: "7"), ImageToDisplay(imageName: "8"),
-//                          ImageToDisplay(imageName: "9"), ImageToDisplay(imageName: "10")]
-//        
-//        collectionView.reloadData()
-//    }
-//    
-//    func setupInitialLayout() {
-//        isGridFlowLayoutUsed = true
-//        self.collectionView.setCollectionViewLayout(PlayersGridFlowLayout, animated: true)
-//    
-//    }
    
-//}
+}
+
+extension GameViewController: UICollectionViewDelegateFlowLayout {
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: collectionView.frame.size.width/3.0 - 8, height: collectionView.frame.size.width/3.0 - 8)
+//    }
+    
+}
---
+++ Coach Timer/Coach Timer/activePlayerCell.swift
@@ -0,0 +1,49 @@
+//
+//  activePlayerCell.swift
+//  Coach Timer
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 4/11/17.
+//  Copyright © 2017 Put Us In Coach. All rights reserved.
+//
+
+import UIKit
+
+class activePlayerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+ @IBOutlet weak var imageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(self.imageView)
+        
+        self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: self.imageView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0))
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.makeItCircle()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func makeItCircle() {
+        self.imageView.layer.masksToBounds = true
+        self.imageView.layer.cornerRadius  = CGFloat(roundf(Float(self.imageView.frame.size.width/2.0)))
+    }    
+}
